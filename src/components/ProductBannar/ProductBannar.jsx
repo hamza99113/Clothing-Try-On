744af34230ad6d92786dc5841d6a12a483efd33f
hamza99113 <alihamza99113@gmail.com>
2023-05-13 02:56:53 +0500

    Final

--- src/components/ProductBannar/ProductBannar.jsx
+++ src/components/ProductBannar/ProductBannar.jsx
@@ -3,9 +3,11 @@ import './ProductBannar.css'
 
 function Banner() {
   return (
+    <div className='bbb'>
     <section id="probanner" className="section-m1">
      <h2><span>#Shopping</span></h2>
     </section> 
+    </div>
   );
 }
 
